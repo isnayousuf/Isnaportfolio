c7e9a451d51eed3fe72b7c9a08623b127682e4e4
isnayousuf77@gmail.com <isnayousuf77@gmail.com>
2023-02-06 20:20:43 +0530

    add the theme button

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,28 +67,36 @@ const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
 const iconTheme = "ri-sun-line";
 
+const sunIcon = document.getElementById('sun-icon')
+
 const selectedTheme = localStorage.getItem("selected-theme");
 const selectedIcon = localStorage.getItem("selected-icon");
 const getCurrentTheme = () =>
   document.body.classList.contains(darkTheme) ? "dark" : "light";
-const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "ri-moon-line " : "ri-sun-line";
+    themeButton.addEventListener('click', function() {
+    
+         sunIcon.style.display= "block"
+         themeButton.style.display = "none";
+    })
 
-if (selectedTheme) {
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-    darkTheme
-  );
-  themeButton.classList[selectedIcon === "ri-moon-line " ? "add" : "remove"](
-    iconTheme
-  );
-}
+    sunIcon.addEventListener('click', function(){
+      sunIcon.style.display= "none"
+      themeButton.style.display = "block";
+      
 
+    })
+  
 themeButton.addEventListener("click", () => {
   document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
   localStorage.setItem("selected-theme", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+sunIcon.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+});
+
+
 
 const sr = ScrollReveal({
   origin: "top",
@@ -234,7 +242,7 @@ const sendEmail = (e)=> {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
       </div>
       <p>
-        Thanks!! for initiating the conversation.
+        Thanks for initiating the conversation.
         
       </p>
         <p>I will get back to you as early as possible.</p>
@@ -259,7 +267,7 @@ const sendEmail = (e)=> {
         <h3 class="error_msg">Couldn't send your message!!</h3>
        
         <div class="emoji-div">
-        <img src="./assets/images/error.jpg" alt="">
+        <img src="./assets/images/error.jpg" alt="oops message failed">
          
          </div>
         <div class="alt-div">
@@ -274,7 +282,7 @@ const sendEmail = (e)=> {
                                     <a href="mailto:isnayousuf77@gmail.com"
                                 class="cv__btn email-btn">
                                 Write to me
-                                <i class="ri-arrow-right-line"></i>
+                                <svg class="svg-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"/></svg>
                                 </a>
           </div>
 
